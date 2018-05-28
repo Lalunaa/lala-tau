4e204f55f1b061566d516b698313e91ba218f9ff
Lalunaa <meeggi2@wp.pl>
2018-05-28 15:29:08 +0200

    salenium_fv

--- film/src/test/java/com/lala/app/SomeSiteTest.java
+++ film/src/test/java/com/lala/app/SomeSiteTest.java
@@ -6,6 +6,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.Random;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,25 +22,18 @@ public class SomeSiteTest {
 
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "lala-tau/film/src/test/java/com/lala/app/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "src/test/java/com/lala/app/chromedriver.exe");
 		driver = new ChromeDriver();
 		baseUrl = "http://automationpractice.com/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
 	}
 	@Ignore
 	@Test
-	public void failTest() throws Exception {
-		driver.get(baseUrl + "/index.php");
-		driver.findElement(By.cssSelector(".login")).click();		
-		driver.findElement(By.cssSelector("#email_create")).clear();
-		driver.findElement(By.cssSelector("#email_create")).sendKeys("niepoprawnyemail");
-		driver.findElement(By.cssSelector("#SubmitCreate")).click();
-		Thread.sleep(5000);
-		assertEquals(true, driver.findElement(By.cssSelector("#create_account_error")).isDisplayed());
-	}
-
-	@Test
 	public void registerTest() throws Exception {
+
+		Random rand = new Random();
+		int  n = rand.nextInt(5000) + 1;
 
 		//login_button
 		driver.get(baseUrl + "/index.php");
@@ -47,7 +41,7 @@ public class SomeSiteTest {
 
 		//create_account
 		driver.findElement(By.cssSelector("#email_create")).clear();
-		driver.findElement(By.cssSelector("#email_create")).sendKeys("poprawnyemail3@lala.com");
+		driver.findElement(By.cssSelector("#email_create")).sendKeys("poprawnyemaill" + n + "@lala.com");
 		driver.findElement(By.cssSelector("#SubmitCreate")).click();
 		Thread.sleep(500);		
 		assertEquals(false, driver.findElement(By.cssSelector("#create_account_error")).isDisplayed());
@@ -102,7 +96,9 @@ public class SomeSiteTest {
 		//postcode
 		driver.findElement(By.cssSelector("#postcode")).clear();
 		driver.findElement(By.cssSelector("#postcode")).sendKeys("88888");
-		assertEquals(5,driver.findElement(By.cssSelector("#postcode")).getAttribute("value").length());
+		//assertEquals(5,driver.findElement(By.cssSelector("#postcode")).getAttribute("value").length());
+		Thread.sleep(500);
+		assertEquals(true, driver.findElement(By.cssSelector("#create_account_error")).isDisplayed());
 		
 		//country
 		driver.findElement(By.cssSelector("#id_country")).click();
@@ -124,6 +120,97 @@ public class SomeSiteTest {
 		Thread.sleep(500);
 		assertEquals(true, driver.findElement(By.cssSelector("div.col-sm-6:nth-child(1) > ul:nth-child(1) > li:nth-child(4) > a:nth-child(1) > span:nth-child(2)")).isDisplayed());
 	}
+
+	@Test
+	public void registerTestFailed() throws Exception {
+
+		Random rand = new Random();
+		int  n = rand.nextInt(5000) + 1;
+
+		//login_button
+		driver.get(baseUrl + "/index.php");
+		driver.findElement(By.cssSelector(".login")).click();
+
+		//create_account
+		driver.findElement(By.cssSelector("#email_create")).clear();
+		driver.findElement(By.cssSelector("#email_create")).sendKeys("poprawnyemaill" + n + "@lala.com");
+		driver.findElement(By.cssSelector("#SubmitCreate")).click();
+		Thread.sleep(500);		
+		assertEquals(false, driver.findElement(By.cssSelector("#create_account_error")).isDisplayed());
+
+		//gender
+		driver.findElement(By.cssSelector("#id_gender2")).click();
+		assertEquals(true,driver.findElement(By.cssSelector("#id_gender2")).isSelected());
+
+		//firstname
+		driver.findElement(By.cssSelector("#customer_firstname")).clear();
+		driver.findElement(By.cssSelector("#customer_firstname")).sendKeys("Magdalena");
+		assertNotEquals(0,driver.findElement(By.cssSelector("#customer_firstname")).getAttribute("value").length());
+		
+		//lastname
+		driver.findElement(By.cssSelector("#customer_lastname")).clear();
+		driver.findElement(By.cssSelector("#customer_lastname")).sendKeys("Lalalalalala");
+		assertNotEquals(0,driver.findElement(By.cssSelector("#customer_lastname")).getAttribute("value").length());
+		
+		//email
+		assertEquals(true,driver.findElement(By.cssSelector("#email")).getAttribute("value").contains(Character.toString('@')));
+		
+		//password
+		driver.findElement(By.cssSelector("#passwd")).clear();
+		driver.findElement(By.cssSelector("#passwd")).sendKeys("haslo");
+		assertNotEquals(0,driver.findElement(By.cssSelector("#passwd")).getAttribute("value").length());
+		
+		//firstname
+		driver.findElement(By.cssSelector("#firstname")).clear();
+		driver.findElement(By.cssSelector("#firstname")).sendKeys("Magdalena");
+		assertNotEquals(0,driver.findElement(By.cssSelector("#firstname")).getAttribute("value").length());
+		
+		//lastname
+		driver.findElement(By.cssSelector("#lastname")).clear();
+		driver.findElement(By.cssSelector("#lastname")).sendKeys("Lalalalalala");
+		assertNotEquals(0,driver.findElement(By.cssSelector("#lastname")).getAttribute("value").length());
+		
+		//address
+		driver.findElement(By.cssSelector("#address1")).clear();
+		driver.findElement(By.cssSelector("#address1")).sendKeys("Adres 101");
+		assertNotEquals(0,driver.findElement(By.cssSelector("#address1")).getAttribute("value"));
+		
+		//city
+		driver.findElement(By.cssSelector("#city")).clear();
+		driver.findElement(By.cssSelector("#city")).sendKeys("Miasto");
+		assertNotEquals(0,driver.findElement(By.cssSelector("#city")).getAttribute("value"));
+		
+		//state
+		driver.findElement(By.cssSelector("#id_state")).click();
+		driver.findElement(By.cssSelector("#id_state > option:nth-child(13)")).click();
+		assertNotEquals(null,driver.findElement(By.cssSelector("#id_state > option:nth-child(13)")).getAttribute("value"));
+		
+		//postcode
+		driver.findElement(By.cssSelector("#postcode")).clear();
+		driver.findElement(By.cssSelector("#postcode")).sendKeys("888888");
+		assertNotEquals(5,driver.findElement(By.cssSelector("#postcode")).getAttribute("value").length());		
+		
+		//country
+		driver.findElement(By.cssSelector("#id_country")).click();
+		driver.findElement(By.cssSelector("#id_country > option:nth-child(2)")).click();
+		assertNotEquals(null,driver.findElement(By.cssSelector("#id_country > option:nth-child(2)")).getAttribute("value"));
+		
+		//phone
+		driver.findElement(By.cssSelector("#phone_mobile")).clear();
+		driver.findElement(By.cssSelector("#phone_mobile")).sendKeys("111222333");
+		assertNotEquals(0,driver.findElement(By.cssSelector("#phone_mobile")).getAttribute("value").length());
+		
+		//alias
+		driver.findElement(By.cssSelector("#alias")).clear();
+		driver.findElement(By.cssSelector("#alias")).sendKeys("Alias");
+		assertNotEquals(0,driver.findElement(By.cssSelector("#alias")).getAttribute("value"));
+		
+		//submit
+		driver.findElement(By.cssSelector("#submitAccount")).click();
+		Thread.sleep(500);
+		assertEquals(true, driver.findElement(By.cssSelector(".alert")).isDisplayed());
+	}
+
 
 	@After
 	public void tearDown() throws Exception {
